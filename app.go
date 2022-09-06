cf0006c1ab5d1483be97b184ca1d3eab3b528ca1
Ali Hamidi <57750952+ahmeroxa@users.noreply.github.com>
2022-09-06 09:22:59 -0700

    initial import

---
+++ app.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+
+	// Dependencies of Turbine
+	"github.com/meroxa/turbine-go"
+	"github.com/meroxa/turbine-go/runner"
+)
+
+func main() {
+	runner.Start(App{})
+}
+
+var _ turbine.App = (*App)(nil)
+
+type App struct{}
+
+func (a App) Run(v turbine.Turbine) error {
+	cck, err := v.Resources("cck")
+	if err != nil {
+		return err
+	}
+
+	rr, err := source.Records("topic_1", nil)
+	if err != nil {
+		return err
+	}
+
+	err = cck.Write(rr, "topic_2")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
